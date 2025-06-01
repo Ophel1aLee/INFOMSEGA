23ae5ab2bf8c669e5f79f00b126c373333e8e69d
yoko <sharpmiao@gmail.com>
2025-06-01 19:05:15 +0200

    After accepting the task, there will be an introduction of the creature

--- Assets/Scripts/UI/ChallengeAccepting.cs
+++ Assets/Scripts/UI/ChallengeAccepting.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ChallengeAccepting : MonoBehaviour
 {
     private GameObject m_uiObject;
     private AnimalData m_currentAnimalData;
+    private GameObject director;
+    private Button goButton;
+    public TextMeshProUGUI contentText;
+    private AnimalData animalInf;
+    private GameObject next;
+    private Button nextInf;
 
     // Start is called before the first frame update
     void Start()
@@ -28,15 +36,35 @@ public class ChallengeAccepting : MonoBehaviour
             if (button.name == "Accept")
             {
                 // Add the button click event
-                button.onClick.AddListener(AcceptChallenge);
+                button.onClick.AddListener(IntroducePlace);
             }
+        }
+
+        var saveData = SaveManager.Instance.CurrentSaveData;
+        int id = saveData.CurrentCollectingID;
+        MainManager.Instance.AnimalData.TryGetValue(id, out animalInf);
+
+        director = m_uiObject.transform.Find("Director")?.gameObject;
+        goButton = director.GetComponentInChildren<Button>();
+        goButton.gameObject.SetActive(false);
+        if (goButton != null)
+        {
+            goButton.onClick.AddListener(AcceptChallenge);
+        }
+
+        next = m_uiObject.transform.Find("Next")?.gameObject;
+        nextInf = next.GetComponentInChildren<Button>();
+        nextInf.gameObject.SetActive(false);
+        if (nextInf != null)
+        {
+            nextInf.onClick.AddListener(IntroduceFood);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void AcceptChallenge()
@@ -45,6 +73,43 @@ public class ChallengeAccepting : MonoBehaviour
         Debug.Log("Challenge Accepted");
         
         MainManager.Instance.NextLevel(ProgressEnum.HabitatChoosing);
+    }
+
+    private void IntroducePlace()
+    {
+        nextInf.gameObject.SetActive(true);
+        string name = animalInf.AnimalName;
+        string habitat = $"{animalInf.AnimalHabitat}";
+        string habitatConcat = " ";
+
+        switch (habitat)
+        {
+            case "Ocean":
+                habitatConcat = "the water is abundant";
+                break;
+        }
+
+        string habitatDescription = string.Concat(name, " are creatures living in a place where ", habitatConcat, ".");
+        contentText.text = habitatDescription;
+    }
+
+    private void IntroduceFood()
+    {
+        next.SetActive(false);
+        goButton.gameObject.SetActive(true);
+
+        string diet = $"{animalInf.AnimalDiet}";
+        string dietConcat = " ";
+
+        switch (diet)
+        {
+            case "Planktivore":
+                dietConcat = "can float in water and move with the current";
+                break;
+        }
+
+        string dietDescription = string.Concat("They feed on something that ", dietConcat, ".");
+        contentText.text = dietDescription;
     }
 
     private void InitChallenge()
